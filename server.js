3eb77ca84c72526ece294419a1019a77864c8713
MGuerrero1899 <mguerrero1899@gmail.com>
2021-08-28 23:37:59 -0700

    Add fetch finnish server json from server
    Added server side fetching of json marker points to the express server

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const path = require('path')
+const path = require('path');
 
 app.set('view engine','ejs');
 //Grabs files from our static directory
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('/',(req,res) => {
-    res.sendFile('index.html')
+    res.sendFile('index.html');
+})
+
+app.get('/finnishvirtual',async (req,res) => {
+    const url = 'http://stats.virtualpilots.fi:8000/static/output.json';
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data)
 })
 
 app.listen(PORT,() => {
-    console.log(`Server Running on PORT ${PORT}`)
+    console.log(`Server Running on PORT ${PORT}`);
 })
 
